402c38d9d58b1e8ef686461094217adfa1ab221f
Alleayy <Daipooran@outlook.com>
2025-06-21 11:14:17 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -8,8 +8,9 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+ html_favicon = ['_static/canon-logo.png']
+
 
 html_js_files = [
     '_static/chat.js',
 ]
- html_favicon = ['_static/canon-logo.png']
